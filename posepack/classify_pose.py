2b82a828748eeeb7e01cc5d0e9eb13d1411b1d18
themdeee <mde819710@gmail.com>
2025-04-28 23:57:37 +0800

    20250428

--- posepack/classify_pose.py
+++ posepack/classify_pose.py
@@ -1,6 +1,140 @@
 import math
 
-DEBUG_MODE = False
+DEBUG_MODE = True
+
+def get_angle(vertex, point1, point2):
+    vector1 = (point1[0] - vertex[0], point1[1] - vertex[1])
+    vector2 = (point2[0] - vertex[0], point2[1] - vertex[1])
+
+    length1 = math.hypot(vector1[0], vector1[1])
+    length2 = math.hypot(vector2[0], vector2[1])
+    if length1 == 0 or length2 == 0:
+        return 0.0
+
+    dot_product = vector1[0] * vector2[0] + vector1[1] * vector2[1]
+    cos_angle = dot_product / (length1 * length2)
+    cos_angle = max(min(cos_angle, 1.0), -1.0)
+    angle = math.degrees(math.acos(cos_angle))
+    
+    return angle
+
+def is_armcrossed(left_elbow, right_elbow, left_wrist, right_wrist):
+    left_elbow_x, left_elbow_y, left_elbow_visibility, left_elbow_presence = left_elbow
+    right_elbow_x, right_elbow_y, right_elbow_visibility, right_elbow_presence = right_elbow    
+    left_wrist_x, left_wrist_y, left_wrist_visibility, left_wrist_presence = left_wrist
+    right_wrist_x, right_wrist_y, right_wrist_visibility, right_wrist_presence = right_wrist
+
+    lenth_left_wrist_to_left_elbow = math.dist([left_wrist_x, left_wrist_y], [left_elbow_x, left_elbow_y])
+    lenth_left_wrist_to_right_elbow = math.dist([left_wrist_x, left_wrist_y], [right_elbow_x, right_elbow_y])
+    lenth_right_wrist_to_left_elbow = math.dist([right_wrist_x, right_wrist_y], [left_elbow_x, left_elbow_y])
+    lenth_right_wrist_to_right_elbow = math.dist([right_wrist_x, right_wrist_y], [right_elbow_x, right_elbow_y])
+    
+    min_presence = min(left_elbow_presence, right_elbow_presence,
+                       left_wrist_presence, right_wrist_presence)
+    
+    if DEBUG_MODE:
+        print("左腕到左肘距离: ", lenth_left_wrist_to_left_elbow)
+        print("左腕到右肘距离: ", lenth_left_wrist_to_right_elbow)
+        print("右腕到左肘距离: ", lenth_right_wrist_to_left_elbow)
+        print("右腕到右肘距离: ", lenth_right_wrist_to_right_elbow)
+        print("最小置信度: ", min_presence)
+
+    if (left_wrist_y < right_elbow_y and
+        right_wrist_y < left_elbow_y and
+        lenth_left_wrist_to_right_elbow < lenth_left_wrist_to_left_elbow and
+        lenth_right_wrist_to_left_elbow < lenth_right_wrist_to_right_elbow):
+
+        return "Armcrossed", min_presence
+    else:
+        return "Unknown", min_presence
+
+def is_bow(left_shoulder, right_shoulder, left_hip, right_hip, left_knee, right_knee, left_ankle, right_ankle):
+    left_shoulder_x, left_shoulder_y, left_shoulder_visibility, left_shoulder_presence = left_shoulder
+    right_shoulder_x, right_shoulder_y, right_shoulder_visibility, right_shoulder_presence = right_shoulder
+    left_hip_x, left_hip_y, left_hip_visibility, left_hip_presence = left_hip
+    right_hip_x, right_hip_y, right_hip_visibility, right_hip_presence = right_hip
+    left_knee_x, left_knee_y, left_knee_visibility, left_knee_presence = left_knee
+    right_knee_x, right_knee_y, right_knee_visibility, right_knee_presence = right_knee
+    left_ankle_x, left_ankle_y, left_ankle_visibility, left_ankle_presence = left_ankle
+    right_ankle_x, right_ankle_y, right_ankle_visibility, right_ankle_presence = right_ankle
+
+    angle_hip_left = get_angle(left_hip, left_shoulder, left_ankle)
+    angle_hip_right = get_angle(right_hip, right_shoulder, right_ankle)
+    angle_knee_left = get_angle(left_knee, left_hip, left_ankle)
+    angle_knee_right = get_angle(right_knee, right_hip, right_ankle)
+
+    min_presence = min(left_shoulder_presence, right_shoulder_presence,
+                       left_hip_presence, right_hip_presence,
+                       left_knee_presence, right_knee_presence,
+                       left_ankle_presence, right_ankle_presence)
+    
+    if DEBUG_MODE:
+        print("左腰角度: ", angle_hip_left)
+        print("右腰角度: ", angle_hip_right)
+        print("左膝盖角度: ", angle_knee_left)
+        print("右膝盖角度: ", angle_knee_right)
+        print("特征点最小置信度:", min_presence)
+
+    if (angle_hip_left > 90 and angle_hip_right > 90 and
+        angle_knee_left > 150 and angle_knee_right > 150):
+
+        return "Bow", min_presence
+    else:
+        return "Unknown", min_presence
+
+def is_handsup_I():
+    return "Handsup-I"
+
+def is_handsup_T():
+    return "Handsup-T"
+
+def is_handsup_Y():
+    return "Handsup-Y"
+
+def is_kneel():
+    return "Kneel"
+
+def is_squart():
+    return "Squat"
+
+def get_pose_test(keypoints):
+    nose = keypoints[0]
+    left_eye_inner = keypoints[1]
+    left_eye = keypoints[2]
+    left_eye_outer = keypoints[3]
+    right_eye_inner = keypoints[4]
+    right_eye = keypoints[5]
+    right_eye_outer = keypoints[6]
+    left_ear = keypoints[7]
+    right_ear = keypoints[8]
+    mouth_left = keypoints[9]
+    mouth_right = keypoints[10]
+    left_shoulder = keypoints[11]
+    right_shoulder = keypoints[12]
+    left_elbow = keypoints[13]
+    right_elbow = keypoints[14]
+    left_wrist = keypoints[15]
+    right_wrist = keypoints[16]
+    left_pinky = keypoints[17]
+    right_pinky = keypoints[18]
+    left_index = keypoints[19]
+    right_index = keypoints[20]
+    left_thumb = keypoints[21]
+    right_thumb = keypoints[22]
+    left_hip = keypoints[23]
+    right_hip = keypoints[24]
+    left_knee = keypoints[25]
+    right_knee = keypoints[26]
+    left_ankle = keypoints[27]
+    right_ankle = keypoints[28]
+    left_heel = keypoints[29]
+    right_heel = keypoints[30]
+    left_foot_index = keypoints[31]
+    right_foot_index = keypoints[32]
+
+    pose, min_presence = is_bow(left_shoulder, right_shoulder, left_hip, right_hip, left_knee, right_knee, left_ankle, right_ankle)
+
+    return pose
 
 def get_pose(keypoints):
     """
